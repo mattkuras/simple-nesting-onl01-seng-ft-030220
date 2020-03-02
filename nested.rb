134f748a24110248c38ae31c7e4fe7374ab7c194
Matt Kuras <mattkuras@gmail.com>
2020-03-02 17:43:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].a_to_s
+     programmer_hash[:dennis_ritchie][:languages].a_tos
 end
 
 def adding_matz
