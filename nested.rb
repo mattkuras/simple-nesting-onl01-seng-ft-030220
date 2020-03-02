04e0c2b8c954565e33f38717d1ed44d4fc78ead9
Matt Kuras <mattkuras@gmail.com>
2020-03-02 17:41:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,7 +38,7 @@ def alan_kay_is_known_for
         }
      }
      
-     
+    programmer_hash[:alan_kay][:known_for] 
 end
 
 def dennis_ritchies_language
@@ -57,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+     
 end
 
 def adding_matz
